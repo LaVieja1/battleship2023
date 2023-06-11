93a403a4b13adc815a71b9a874dbac9ab0e03d02
santicano <canosantiago404@gmail.com>
2023-06-11 12:21:41 -0300

    Añadida funcion de agregar barco con su test

--- src/modules/gameboard.js
+++ src/modules/gameboard.js
@@ -5,8 +5,56 @@ const Gameboard = () => {
     let board = Array(10).fill(null).map(() => Array(10).fill(null));
     const getBoard = () => board;
 
+    let placedShips = [];
+    const areAllShipsPlaces = () => placedShips.length === SHIP_TYPES.length;
+
+    const adjustCoords = (y0, x0, i, direction) => {
+        //Default - Horizontal
+        let x = x0 + i;
+        let y = y0;
+        if (direction === 'vertical') {
+            x = x0;
+            y = y0 + i;
+        }
+        return [y, x];
+    };
+
+    //Coloca el barco en las coordenadas (y, x);
+    const placeShip = (ship, y0, x0) => {
+        const direction = ship.getDirection();
+        //Verifica si esta fuera del tablero o choca con otro barco
+        const valid = checkValid(ship.length, direction, y0, x0);
+        if (valid) {
+            for (let i = 0; i < ship.length; i++) {
+                const [y, x] = adjustCoords(y0, x0, i, direction);
+                //Coloca el barco en el index
+                board[y][x] = { ship, index: i };
+            }
+            //Lo agrega a placedShips
+            placedShips.push(ship);
+            return valid;
+        } else {
+            return valid;
+        }
+    };
+
+    const checkValid = (length, direction, y0, x0) => {
+        const cells = [];
+        for (let i = 0; i < length; i++) {
+            const [y, x] = adjustCoords(y0, x0, i, direction);
+            //Verifica si y/x esta fuera del tablero
+            if (y < 10 && x < 10) {
+                cells.push(board[y][x]);
+            } else {
+                return false;
+            }
+        }
+        return cells.every((cell) => cell === null);
+    };
+
     return {
         getBoard,
+        placeShip,
     };
 }
 
--- src/tests/gameboard.test.js
+++ src/tests/gameboard.test.js
@@ -1,4 +1,5 @@
 import Gameboard from "../modules/gameboard.js";
+import Ship from "../modules/ship.js";
 
 describe('Gameboard', () => {
     describe('Board', () => {
@@ -23,5 +24,49 @@ describe('Gameboard', () => {
         });
     });
 
+    describe('Place horizontal ship', () => {
+        const gameboard = Gameboard();
+        const ship = Ship('cruiser');
+        gameboard.placeShip(ship, 3, 2);
 
+        test('Placed ship at starter coord index: 0', () => {
+            const actual = gameboard.getBoard()[3][2];
+            expect(actual).toEqual({ ship, index: 0 })
+        });
+
+        test('Placed ship at coord index: 1', () => {
+            const actual = gameboard.getBoard()[3][3];
+            expect(actual).toEqual({ ship, index: 1 });
+        });
+
+        test('Placed ship at final coord index: 2', () => {
+            const actual = gameboard.getBoard()[3][4];
+            expect(actual).toEqual({ ship, index: 2 });
+        });
+    });
+
+    describe('Place vertical ship', () => {
+        const gameboard = Gameboard();
+        const ship = Ship('submarine');
+        ship.changeDirection();
+        gameboard.placeShip(ship, 3, 2);
+
+       
+        test('Placed ship at starter coord index: 0', () => {
+            const actual = gameboard.getBoard()[3][2];
+            expect(actual).toEqual({ ship, index: 0 })
+        });
+
+        test('Placed ship at coord index: 1', () => {
+            const actual = gameboard.getBoard()[4][2];
+            expect(actual).toEqual({ ship, index: 1 });
+        });
+
+        test('Placed ship at final coord index: 2', () => {
+            const actual = gameboard.getBoard()[5][2];
+            expect(actual).toEqual({ ship, index: 2 });
+        }); 
+    });
+
+    
 })
